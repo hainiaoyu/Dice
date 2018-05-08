5d6d35b67acb817b0bddda2d95c47a738e3cd5cd
hainiaoyu <hli47@stevens.edu>
2018-05-07 18:52:14 -0700

    Add layouts for the main screen. Add listener to button.

--- app/src/main/java/com/example/yimingli/dice/MainActivity.java
+++ app/src/main/java/com/example/yimingli/dice/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.yimingli.dice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button rollButton = (Button) findViewById(R.id.rollButton);
+
+        ImageView image_left = (ImageView) findViewById(R.id.image_dice_left);
+
+        ImageView image_right = (ImageView) findViewById(R.id.image_dice_right);
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // log.d means to set debug log
+                Log.d("Dice", "Hello");
+            }
+        });
     }
 }
