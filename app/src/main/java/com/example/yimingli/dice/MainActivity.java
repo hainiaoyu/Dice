e64bad804a474f149fce4cd2a9cb800cf19a0a6a
hainiaoyu <hli47@stevens.edu>
2018-05-08 09:59:25 -0700

    Generate random number; Use int array to store images id; Set image resource.

--- app/src/main/java/com/example/yimingli/dice/MainActivity.java
+++ app/src/main/java/com/example/yimingli/dice/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -14,17 +16,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final int[] diceArray = {
+                R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6,
+        };
+
         Button rollButton = (Button) findViewById(R.id.rollButton);
 
-        ImageView image_left = (ImageView) findViewById(R.id.image_dice_left);
+        final ImageView image_left = (ImageView) findViewById(R.id.image_dice_left);
 
-        ImageView image_right = (ImageView) findViewById(R.id.image_dice_right);
+        final ImageView image_right = (ImageView) findViewById(R.id.image_dice_right);
 
         rollButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // log.d means to set debug log
+                // Testing log.d means to set debug log
                 Log.d("Dice", "Hello");
+
+                // Generate a random number
+                Random randomGenerator = new Random();
+                int number1 = randomGenerator.nextInt(6);
+                int number2 = randomGenerator.nextInt(6);
+
+                image_left.setImageResource(diceArray[number1]);
+                image_right.setImageResource(diceArray[number2]);
             }
         });
     }
